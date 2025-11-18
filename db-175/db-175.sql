675961e2811f86cb034dfaa11a90ac95b02066f9
arc-1409 <lulu.august.liao@gmail.com>
2025-11-17 23:30:12 -0500

    add right join version

--- db-175/db-175.sql
+++ db-175/db-175.sql
@@ -1,2 +1,7 @@
 select p.firstname, p.lastname, a.city, a.state from person p left join address a 
 on p.personID = a.personID;
+
+/* alternatively, with right join */
+
+select p.firstname, p.lastname, a.city, a.state from address a right join person p 
+on a.personID = p.personID; 
