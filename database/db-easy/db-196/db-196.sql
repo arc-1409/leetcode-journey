12780782631097fbdf4ab325f2d9fbcee821137e
arc-1409 <lulu.august.liao@gmail.com>
2025-11-18 22:35:17 -0500

    delete

--- database/db-easy/db-196/db-196.sql
+++ database/db-easy/db-196/db-196.sql
@@ -1,1 +1,1 @@
-select p1 from person p1 inner join person p2 on p1.email = p2.email where p1.id > p2.id; 
+delete p1 from person p1 inner join person p2 on p1.email = p2.email where p1.id > p2.id; 
